c351371187f16f25a3c28c2d24f2f5af8678d59d
Sardor <sardorsher1337@gmail.com>
2023-02-27 21:42:00 +0500

    created controller

---
+++ eTicketCinema/eTicketCinema.Mvc/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using eTicketCinema.Mvc.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace eTicketCinema.Mvc.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _dbcontext;
+        public MoviesController(AppDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allPMovies = await _dbcontext.Movies.ToListAsync();
+            return View();
+        }
+    }
+}
---
+++ eTicketCinema/eTicketCinema.Mvc/Controllers/ProducersController.cs
@@ -0,0 +1,23 @@
+﻿using eTicketCinema.Mvc.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace eTicketCinema.Mvc.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _dbcontext;
+
+        public ProducersController(AppDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allProducer = await _dbcontext.Producers.ToListAsync();
+            return View();
+        }
+    }
+}
